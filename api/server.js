a07d4c151bb98a4c63d9ea664570d7fabe362948
Darley Peralta <ordeal00s@gmail.com>
2023-08-20 19:44:22 -0400

    adding validation

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
+const {logger} = require('./middleware/middleware')
 const server = express();
+const userRouter = require('./users/users-router')
+server.use(express.json())
+server.use(logger)
 
-// remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
